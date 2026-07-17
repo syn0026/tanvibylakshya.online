7104659b386715ac41c7b1209514765c9100c76f
syn0026 <sayaneshas0026@gmail.com>
2026-07-17 20:32:25 +0530

    c:\Users\sayan\Downloads\Sooryakanthi.png

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ if(nform) {
 const collectionsDB = {
   'setmundu': [
     { title: "Hemangi", price: "999", originalPrice: "1299", img: "assets/hemant.png" },
-    { title: "Sooryakanthi", price: "6,200", originalPrice: "7000", img: "assets/suriyakathi.png" }
+    { title: "Sooryakanthi", price: "6,200", originalPrice: "7000", img: "assets/Sooryakanthi.png" }
   ],
   'malkotta': [
     { title: "Pure Malkotta Cotton Saree", price: "3,800", originalPrice: "4,500", img: "assets/malkotta-saree.png" },
