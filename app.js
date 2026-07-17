a54b81c02bc678ce3a4d4bc3f76015e3e326768b
syn0026 <sayaneshas0026@gmail.com>
2026-07-17 20:22:12 +0530

    edit

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ if(nform) {
 // You can easily update prices, titles, and image paths here!
 const collectionsDB = {
   'setmundu': [
-    { title: "PREMIUM SET MUND HEMANGI", price: "999", originalPrice: "1299", img: "assets/hemangi.png" },
+    { title: "PREMIUM SET MUND HEMANGI", price: "999", originalPrice: "1299", img: "assets/hemant.png" },
     { title: "Handloom Mural Painted Set", price: "6,200", originalPrice: "7000", img: "assets/setummund.png" }
   ],
   'malkotta': [
